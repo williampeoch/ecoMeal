51010bc033c558fcfec0f0968a7149495ae7d408
William Peoc'h <peoch.william44@gmail.com>
2025-03-11 09:38:04 +0100

    add Person class and test.ipynb to calculate meals

--- Person.py
+++ Person.py
@@ -7,6 +7,17 @@ class Person:
         self.age = age
         self.level_activity = level_activity
 
-    def calculate_NAP(self):
-        NAP_values = {'0': 1.2, '1': 1.4, '2': 1.5, '3': 1.7, '4': 1.8, '5': 2.1, '6': 2.4}
-        return NAP_values[self.level_activity]
+    def calculate_PAL(self):
+        # Physical Activity Level
+        PAL_values = {'0': 1.2, '1': 1.4, '2': 1.5, '3': 1.7, '4': 1.8, '5': 2.1, '6': 2.4}
+        return PAL_values[self.level_activity]
+    
+    def calculate_BMR(self):
+        # Calculate Basal Metabolic Rate with Mifflin-St Jeor Equation
+        if self.sex == "male":
+            return 10*self.weight + 6.25*self.height - 5*self.age + 5
+        else:
+            return 10*self.weight + 6.25*self.height - 5*self.age - 161
+        
+    def calculate_needs(self):
+        return self.calculate_BMR() * self.calculate_PAL()
---
+++ main.py
@@ -0,0 +1,4 @@
+
+
+will = Person("male", 62, 173, 22, '2')
+will.calculate_needs()
