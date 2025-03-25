4faafd937c53953913fe1ced2685a56c0d14d52e
Ricard Melanie <mricard@bs406-s33-02.insa-lyon.fr>
2025-03-25 11:21:33 +0100

    MAJ des datasets

--- test_tp2.py
+++ test_tp2.py
@@ -1,4 +1,14 @@
 import pandas as pd
 
-FAO = pd.read_excel("4-TableS1_augmented_with_FAO_data.xlsx")
-print(FAO.describe())
+nom_fichier = "data/ecological_data.xlsx"
+nom_feuille = "ecodata"
+
+# Charger l'Excel
+df = pd.read_excel(nom_fichier, sheet_name=nom_feuille)
+
+# Vérifier l'importation
+print("j'affiche l'entete" )
+print(df.head())
+
+# Afficher les colonnes après transformation
+print(df.columns)
