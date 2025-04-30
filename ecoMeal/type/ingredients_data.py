eea3a404a10c48e327078f183c7c5fb84da5579f
Linh <103323845+i-Linh@users.noreply.github.com>
2025-04-30 19:10:10 +0200

    bug des unités + check des inputs

--- ecoMeal/type/ingredients_data.py
+++ ecoMeal/type/ingredients_data.py
@@ -5,13 +5,13 @@ from ecoMeal.type.ingredient import Ingredient
 from ecoMeal.type.meal import Meal
 
 class IngredientData:
-	def __init__(self, ingredient_by_type):
-		self.ingredient_by_type=ingredient_by_type
+    def __init__(self, ingredient_by_type):
+        self.ingredient_by_type=ingredient_by_type
 
-	def generate_meal(self):
-		meal_ingredients=[]
-		for type, ingredients_list in self.ingredient_by_type.items():
-			meal_ingredients.append(random.choice(ingredients_list))
-		return Meal(meal_ingredients) 
+    def generate_meal(self):
+        meal_ingredients=[]
+        for type, ingredients_list in self.ingredient_by_type.items():
+            meal_ingredients.append(random.choice(ingredients_list))
+        return Meal(meal_ingredients) 
 
 
