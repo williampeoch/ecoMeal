dc539731a99c4c85d58085579bc7a396b51c2dd6
Linh <103323845+i-Linh@users.noreply.github.com>
2025-05-08 19:06:24 +0200

    bug réglé

--- ecoMeal/ecoscore.py
+++ ecoMeal/ecoscore.py
@@ -1,5 +1,7 @@
 import numpy as np
+import math
 
+from ecoMeal.simplexe import calculate_meal_quantities_for_user_needs
 from ecoMeal.type.ingredients_data import IngredientData
 
 def ecoscore_per_meal(meal, quantities):
@@ -7,34 +9,44 @@ def ecoscore_per_meal(meal, quantities):
     # score total repas : quantité  ingrédient 1 * score de l'ingrédient (pondération indicateur1 * valeur indicateur6 par unité + ... + pondération indicateur6 * valeur indicateur6 par unité)
     #                    + ...
     #                 +   quantité  ingrédient 6 * score de l'ingrédient (pondération indicateur1 * valeur indicateur6 par unité + ... + pondération indicateur6 * valeur indicateur6 par unité)
-    return float(np.dot(ecoscore_vector, quantities)) # score total repas 
+    return float(np.dot(ecoscore_vector, quantities)) # score total repas
 
+def get_best_and_interval_ecoscore(ingredients_data, needs):
+    best_ecoscore_value = math.inf
+    worst_ecoscore_value = 0
 
+    for i in reversed(range(len(ingredients_data.all_possible_meals))):
+        meal = ingredients_data.all_possible_meals[i]
+        meal.quantities = calculate_meal_quantities_for_user_needs(meal.get_matrix(), needs)
 
+        # le else est executé uniquement si la boucle ne break pas
+        for quantity in meal.quantities:
+            if quantity == 0 :
+                del ingredients_data.all_possible_meals[i]
+                break
+        else:
+            meal_ecoscore = ecoscore_per_meal(meal, meal.quantities)
+            meal.ecoscore = meal_ecoscore
 
-# calcul de l'impact total par indicateur pour un repas :
-# somme_pour_un_indicateur : indicateur * quantité de l'ingrédient
+            if meal_ecoscore < best_ecoscore_value:
+                best_ecoscore_value = meal_ecoscore
+            if meal_ecoscore > worst_ecoscore_value:
+                worst_ecoscore_value = meal_ecoscore
 
+    ecoscore_interval = worst_ecoscore_value - best_ecoscore_value
 
+    return best_ecoscore_value, ecoscore_interval
 
+def get_valid_meals(ingredients_data, needs, ecoscore_importance):
+    valid_meals = [] # liste de tous les repas correspondant au choix de l'utilisateur
+    best_ecoscore_value, ecoscore_interval = get_best_and_interval_ecoscore(ingredients_data, needs)
+    ecoscore = ["A", "B", "C", "D", "E"]
 
-
-
-# meilleur repas : quantité  ingrédient + score de l'ingrédient
-
-# normalisation de chaque indicateur
-# valeur de référence = basé sur la moyenne de chaque indictateur pour l'ensemble des repas générés ? (au delà d'un certain seuil : 100)
-#
-# score normalisé de chaque indicateur : somme_pour_un_indicateur / valeur de référence
-#
-# calcul de l'écoscore global
-# score global = score normalisé de chaque indicateur * pondération des indicateurs (optionnel, fixé à 1)
-#
-# Echelle pour attribuer un ecoscore normalisé de 1 à 5 (pour correspondre à la note de l'utilisateur) : 0 il s'en fout et skip juste
-# <20e percentile 5
-# 20-40e percentile 4
-# 40-60e percentile 3
-# 60-80e percentile 2
-# >80e 1
-#
-# plus l'écoscore global est faible meilleur est le repas = note de 5
+    for meal in ingredients_data.all_possible_meals:
+        meal_ecoscore_index = math.floor((((meal.ecoscore-best_ecoscore_value)*100)/ecoscore_interval)/20)
+        if meal_ecoscore_index == 5:
+            meal_ecoscore_index = 4 # pour le worst_ecoscore_meal, le floor donne 5, qui n'est pas dans la range 0 - 4 de la liste ecoscore
+        meal.ecoscore_letter = ecoscore[meal_ecoscore_index]
+        if meal_ecoscore_index <= ecoscore_importance:
+            valid_meals.append(meal)
+    return valid_meals
--- ecoMeal/main.py
+++ ecoMeal/main.py
@@ -4,11 +4,10 @@ import math
 import random
 
 from ecoMeal.simplexe import calculate_meal_quantities_for_user_needs
-from ecoMeal.user import user_information
-from ecoMeal.user import get_user_needs
+from ecoMeal.user import user_information, get_user_needs
 from ecoMeal.output_menu import generate_output
 from ecoMeal.load import load_data
-from ecoMeal.ecoscore import ecoscore_per_meal
+from ecoMeal.ecoscore import ecoscore_per_meal, get_valid_meals
 
 def main():
 
@@ -21,44 +20,14 @@ def main():
 
     print("Génération des repas en cours ...")
 
-    best_ecoscore_value = math.inf
-    worst_ecoscore_value = 0
-    best_ecoscore_meal = None
-    worst_ecoscore_meal = None
 
-    for meal in ingredients_data.all_possible_meals:
-        meal.quantities = calculate_meal_quantities_for_user_needs(meal.get_matrix(), needs)
-        meal_ecoscore = ecoscore_per_meal(meal, meal.quantities)
-        meal.ecoscore = meal_ecoscore
 
-        if meal_ecoscore < best_ecoscore_value:
-            best_ecoscore_value = meal_ecoscore
-            best_ecoscore_meal = meal
-        if meal_ecoscore > worst_ecoscore_value:
-            worst_ecoscore_value = meal_ecoscore
-            worst_ecoscore_meal = meal
-
-    ecoscore_interval = worst_ecoscore_value - best_ecoscore_value
-    ecoscore = ["A", "B", "C", "D", "E"]
-
-    valid_meals = [] # liste de tous les repas correspondant au choix de l'utilisateur
-
-    for meal in ingredients_data.all_possible_meals:
-        meal_ecoscore_index = math.floor((((meal.ecoscore-best_ecoscore_value)*100)/ecoscore_interval)/20)
-        if meal_ecoscore_index == 5:
-            meal_ecoscore_index = 4 # pour le worst_ecoscore_meal, le floor donne 5, qui n'est pas dans la range 0 - 4 de la liste ecoscore
-        meal.ecoscore_letter = ecoscore[meal_ecoscore_index]
-        if meal_ecoscore_index <= ecoscore_importance:
-            valid_meals.append(meal)
-
-    if len(valid_meals)==0:
-        print("Aucun repas ne correspond aux critères environnementaux et à vos besoins")
-        return
-
+    valid_meals = get_valid_meals(ingredients_data, needs, ecoscore_importance)
     dataframe_list=[]
 
     if number_of_meals >= len(valid_meals):
         chosen_meal = valid_meals
+        print("Le nombre de repas souhaité dépasse le nombre de repas valides")
     else :
         chosen_meal = random.sample(valid_meals, number_of_meals)
 
